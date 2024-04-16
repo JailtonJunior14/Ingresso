c8c3bc2f5b7d7036a84ce89e461d512b8f1a4087
JailtonJUnior14 <jailtonjrbr@gmail.com>
2024-04-16 12:26:40 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ingresso
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int lote;
+
+            Console.WriteLine("Qual lote  ");
+            Console.WriteLine("");
+
+
+        }
+    }
+}
