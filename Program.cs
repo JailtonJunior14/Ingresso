c31474cd2ec7fcd8f6f0e151ba652aa1874d051d
Jailton Gomes de Carvalho Junior <132059307+JailtonJunior14@users.noreply.github.com>
2024-04-21 10:52:35 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -11,12 +11,69 @@ namespace Ingresso
     {
         static void Main(string[] args)
         {
-            int lote;
-
-            Console.WriteLine("Qual lote  ");
-            Console.WriteLine("");
-
-
+            int contM =0, contI =0;
+		   double contVM =0, contVI =0;
+		    for (int lote=1; lote <= 30; lote++) 
+			{
+                if (lote <= 10) 
+				{
+                    Console.WriteLine("Meia[M] Inteira[I] Lote I");
+					string ing = Console.ReadLine().ToLower().Substring(0,1);
+					switch (ing) {
+                        case "m":
+						    contM++;
+						    contVM += 100/2;
+						break;
+						case "i":
+						    contI++;
+							contVI+= 100;
+						break;
+						default:
+						    Console.WriteLine("Opção invalida");
+						break;
+                    }
+                }
+				else if (lote <= 20) 
+				{
+                    Console.WriteLine("Meia[M] Inteira[I] Lote II");
+					string ing = Console.ReadLine().ToLower().Substring(0,1);
+					switch (ing) {
+                        case "m":
+						    contM++;
+						    contVM += 150/2;
+						break;
+						case "i":
+						    contI++;
+							contVI+= 150;
+						break;
+						default:
+						    Console.WriteLine("Opção invalida");
+						break;
+                    }
+                }
+				else if (lote <=30)
+				{
+                    Console.WriteLine("Meia[M] Inteira[I] Lote III");
+					string ing = Console.ReadLine().ToLower().Substring(0,1);
+					switch (ing) {
+                        case "m":
+						    contM++;
+						    contVM += 200/2;
+						break;
+						case "i":
+						    contI++;
+							contVI+= 200;
+						break;
+						default:
+						    Console.WriteLine("Opção invalida");
+						break;
+                    }
+                }
+            }
+			Console.WriteLine("Total faturado: " + (contVM+contVI).ToString("C"));
+			Console.WriteLine("Total meia: " + contVM.ToString("C"));
+			Console.WriteLine("Total inteira: " + contVI.ToString("C"));
+			Console.ReadKey();
         }
     }
 }
